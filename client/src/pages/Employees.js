c9927dd6157ed6bb5e21f82b9ed42b069f74029f
Simon Liebold <lieboldsimonpaul@gmail.com>
2025-05-21 20:24:06 +0300

    token now creates a modal

--- client/src/pages/Employees.js
+++ client/src/pages/Employees.js
@@ -103,6 +103,8 @@ const EmployeeModal = () => {
     setToken()
     fetchAllEmployments()
     fetchAllJobs()
+    setShowDeleteModal()
+    setShowTokenModal()
 
     try {
       const response = await axios.get(
@@ -190,6 +192,7 @@ const EmployeeModal = () => {
       if (error.response.data.error) addAlert(error.response.data.error)
     }
     setIsTokenLoading(false)
+    setShowTokenModal(true)
   }
 
   const deleteEmployee = async () => {
@@ -226,15 +229,14 @@ const EmployeeModal = () => {
 
   const [showDeleteModal, setShowDeleteModal] = useState(false)
   const DeleteModal = () => {
-    const [confirm, setConfirm] = useState()
+    const [confirm, setConfirm] = useState("")
     return (
       <Modal
         show={showDeleteModal}
         onHide={(e) => setShowDeleteModal(false)}
         backdrop="static"
         keyboard={false}
-        className="modal-2"
-        style={{ zIndex: 1057 }}
+        fullscreen={"md-down"}
       >
         <Modal.Header closeButton>
           <Modal.Title>
@@ -254,7 +256,8 @@ const EmployeeModal = () => {
           <Form.Control
             className="mt-3"
             placeholder={employee.initials}
-            onChange={(e) => setConfirm(e.target.value)}
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value.toUpperCase())}
           ></Form.Control>
         </Modal.Body>
 
@@ -280,7 +283,60 @@ const EmployeeModal = () => {
     )
   }
 
+  const [showTokenModal, setShowTokenModal] = useState(false)
+  const TokenModal = () => {
+    return (
+      token && (
+        <Modal
+          show={showTokenModal}
+          onHide={(e) => setShowTokenModal(false)}
+          backdrop="static"
+          keyboard={false}
+          fullscreen={"md-down"}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <Badge className="me-2">
+                <FontAwesomeIcon icon={faKey} className="me-2" />
+                {token.code}
+              </Badge>{" "}
+              Account-Token
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row>
+              <Col sm="8">
+                Name: {employee.name}
+                <br />
+                <a href={"http://localhost:3001/credentials/" + token.code}>
+                  {"http://localhost:3001/credentials/" + token.code}
+                </a>
+                <hr />
+                E-Mail und Passwort können mit diesem Token festgelegt werden.
+                <br />
+                Bitte scanne den QR-Code oder gehe auf den obenstehenden Link.
+              </Col>
+              <Col sm="4" className="">
+                <QRCode
+                  as={Card.Image}
+                  className="w-100 m-2"
+                  size={150}
+                  value={"http://localhost:3001/credentials/" + token.code}
+                />
+              </Col>
+            </Row>
+          </Modal.Body>
+
+          <Modal.Footer>
+            Gültig bis: {new Date(token.expiresAt).toLocaleString("de-DE")}
+          </Modal.Footer>
+        </Modal>
+      )
+    )
+  }
+
   if (!employee) return
+  if (showTokenModal) return <TokenModal />
   if (showDeleteModal) return <DeleteModal />
   return (
     <>
@@ -313,14 +369,6 @@ const EmployeeModal = () => {
                 {employee.initials}
               </Badge>{" "}
               {employee.name}
-              <Button
-                as={FontAwesomeIcon}
-                variant="danger"
-                size="xs"
-                className="ms-3"
-                icon={faTrash}
-                onClick={(e) => setShowDeleteModal(true)}
-              />
             </Modal.Title>
           )}
         </Modal.Header>
@@ -421,58 +469,6 @@ const EmployeeModal = () => {
               </Button>
             )}
             <hr />
-            <h2 className="fs-6 mt-3">Account-Daten ändern</h2>
-            {!token && (
-              <Button
-                onClick={createToken}
-                variant="primary"
-                disabled={isTokenLoading}
-              >
-                <FontAwesomeIcon className="me-2" icon={faKey} />
-                {isTokenLoading && <>Token lädt...</>}
-                {!isTokenLoading && <>Neuen Token generieren</>}
-              </Button>
-            )}
-            {token && (
-              <Card bg="" text="">
-                <Card.Body>
-                  <Row>
-                    <Col>
-                      <Card.Title>
-                        <Badge className="me-2">
-                          <FontAwesomeIcon icon={faKey} className="me-2" />
-                          {token.code}
-                        </Badge>
-                        Account-Daten Token
-                      </Card.Title>
-                      E-Mail und Passwort können über den QR-Code festgelegt
-                      werden:
-                      <br />
-                      <a
-                        href={"http://localhost:3001/credentials/" + token.code}
-                      >
-                        {"http://localhost:3001/credentials/" + token.code}
-                      </a>
-                    </Col>
-                    <Col sm="auto" className="order-first order-sm-last">
-                      <QRCode
-                        as={Card.Image}
-                        className="w-100 m-2"
-                        size={100}
-                        value={
-                          "http://localhost:3001/credentials/" + token.code
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Card.Body>
-                <Card.Footer>
-                  Gültig bis:{" "}
-                  {new Date(token.expiresAt).toLocaleString("de-DE")}
-                </Card.Footer>
-              </Card>
-            )}
-            <hr />
             <h2 className="fs-6 mt-3">Arbeitsplanung</h2>
             {(isLoading || works.length === 0) && (
               <Alert variant="secondary">
@@ -487,6 +483,16 @@ const EmployeeModal = () => {
             </Row>
           </Form>
         </Modal.Body>
+        <Modal.Footer className="justify-content-end">
+          <Button variant="primary" onClick={createToken}>
+            <FontAwesomeIcon className="me-2" icon={faKey} />
+            {isTokenLoading && <>Token lädt...</>}
+            {!isTokenLoading && <>Token generieren</>}
+          </Button>
+          <Button variant="secondary" onClick={(e) => setShowDeleteModal(true)}>
+            <FontAwesomeIcon className="me-1" icon={faTrash} /> Löschen
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   )
