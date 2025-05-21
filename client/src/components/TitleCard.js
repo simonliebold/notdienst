45611aee365525b87ecbf453a5e5aad49c9fc8f8
Simon Liebold <lieboldsimonpaul@gmail.com>
2025-05-21 20:24:07 +0300

    added titlecard

---
+++ client/src/components/TitleCard.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Card from "react-bootstrap/Card"
+import { Link } from "react-router-dom"
+import Badge from "./Badge"
+
+function TitleCard({ resource, resourceName, icon }) {
+  const { id, short, title } = resource || {}
+
+  return (
+    <Card>
+      <Card.Body
+        as={Link}
+        to={resourceName + id}
+        className="text-decoration-none"
+      >
+        <Badge className="me-2" icon={icon}>
+          {short}
+        </Badge>
+        {title}
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default TitleCard
--- client/src/pages/admin/Elements.js
+++ client/src/pages/admin/Elements.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Badge from "../../components/Badge"
 import { faHashtag, faUser } from "@fortawesome/free-solid-svg-icons"
+import TitleCard from "../../components/TitleCard"
 
 function Elements() {
   return (
@@ -9,6 +10,11 @@ function Elements() {
       <Badge icon={faHashtag} className="">
         201
       </Badge>
+      <h2>TitleCard</h2>
+      <TitleCard
+        icon={faUser}
+        resource={{ id: 256, short: "OKT 2023", title: "Oktober 2023" }}
+      />
     </>
   )
 }
