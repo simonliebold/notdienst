8c2ae5685221ae56f3b4b362327d4b7475b5d35f
Simon Liebold <lieboldsimonpaul@gmail.com>
2025-05-21 20:24:12 +0300

    added breadcrumb to detailed card

--- client/src/components/DetailedCard.js
+++ client/src/components/DetailedCard.js
@@ -15,6 +15,7 @@ import {
 import useDelete from "../hooks/useDelete"
 import { ConfirmDeletePopup } from "./Popup"
 import useSave from "../hooks/useSave"
+import Breadcrumb from "./Breadcrumb"
 
 export const ConfirmDelete = ({ resource, resourceName }) => {
   const [show, open, close, submit, deleting, input, onInput, disabled] =
@@ -60,74 +61,80 @@ const DetailedCard = ({
 
   if (loading || saving)
     return (
-      <Card>
-        <Placeholder as={Card.Header} animation="glow">
-          <Placeholder bg="secondary" xs={2} size="lg" className="me-2" />
-          <Placeholder bg="secondary" xs={5} size="lg" />
-        </Placeholder>
-        <Placeholder as={Card.Body} animation="glow">
-          <Placeholder bg="secondary" xs={4} size="lg" />
-        </Placeholder>
+      <>
+        <Breadcrumb resourceName={resourceName} resource={resource} />
+        <Card>
+          <Placeholder as={Card.Header} animation="glow">
+            <Placeholder bg="secondary" xs={2} size="lg" className="me-2" />
+            <Placeholder bg="secondary" xs={5} size="lg" />
+          </Placeholder>
+          <Placeholder as={Card.Body} animation="glow">
+            <Placeholder bg="secondary" xs={4} size="lg" />
+          </Placeholder>
 
-        <Card.Footer className="d-flex justify-content-end">
-          <Placeholder.Button>
-            <Spinner
-              animation="border"
-              role="status"
-              size={"sm"}
-              className="me-2"
-            ></Spinner>
-            {saving && "Speichert..."}
-            {loading && "Aktualisiert..."}
-          </Placeholder.Button>
-        </Card.Footer>
-      </Card>
+          <Card.Footer className="d-flex justify-content-end">
+            <Placeholder.Button>
+              <Spinner
+                animation="border"
+                role="status"
+                size={"sm"}
+                className="me-2"
+              ></Spinner>
+              {saving && "Speichert..."}
+              {loading && "Aktualisiert..."}
+            </Placeholder.Button>
+          </Card.Footer>
+        </Card>
+      </>
     )
 
   return (
-    <Card className={"text-decoration-none " + className}>
-      <Card.Header
-        onClick={(e) => {
-          if (resource?.works?.length !== 0) setExpanded(!expanded)
-        }}
-        className="fs-6 m-0 d-flex align-items-center justify-content-between"
-      >
-        <div>
-          <Badge
-            resourceName={resourceName}
-            resource={resource}
-            className="me-2"
-          />
-          {title}
-          {edit && " bearbeiten "}
-        </div>
-        {edit && <CloseButton onClick={() => setEdit(false)} />}
-        {!edit && resource?.works?.length !== 0 && (
-          <ExpandButton expanded={expanded} />
+    <>
+      <Breadcrumb resourceName={resourceName} resource={resource} />
+      <Card className={"text-decoration-none " + className}>
+        <Card.Header
+          onClick={(e) => {
+            if (resource?.works?.length !== 0) setExpanded(!expanded)
+          }}
+          className="fs-6 m-0 d-flex align-items-center justify-content-between"
+        >
+          <div>
+            <Badge
+              resourceName={resourceName}
+              resource={resource}
+              className="me-2"
+            />
+            {title}
+            {edit && " bearbeiten "}
+          </div>
+          {edit && <CloseButton onClick={() => setEdit(false)} />}
+          {!edit && resource?.works?.length !== 0 && (
+            <ExpandButton expanded={expanded} />
+          )}
+        </Card.Header>
+        {expanded && (
+          <>
+            <Card.Body>{children}</Card.Body>
+            <Card.Footer className="d-flex justify-content-end align-items-center">
+              {edit && (
+                <>
+                  <ConfirmDelete
+                    resource={resource}
+                    resourceName={resourceName}
+                  />
+                  <CardSaveButton onClick={onSaveRequest} />
+                </>
+              )}
+              {!edit && (
+                <>
+                  <CardEditButton onClick={() => setEdit(true)} />
+                </>
+              )}
+            </Card.Footer>
+          </>
         )}
-      </Card.Header>
-      {expanded && (
-        <>
-          <Card.Body>{children}</Card.Body>
-          <Card.Footer className="d-flex justify-content-end align-items-center">
-            {edit && (
-              <>
-                <ConfirmDelete
-                  resource={resource}
-                  resourceName={resourceName}
-                />
-                <CardSaveButton onClick={onSaveRequest} />
-              </>
-            )}
-            {!edit && (
-              <>
-                <CardEditButton onClick={() => setEdit(true)} />
-              </>
-            )}
-          </Card.Footer>
-        </>
-      )}
-    </Card>
+      </Card>
+    </>
   )
 }
 
