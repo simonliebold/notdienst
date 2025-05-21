6abbd8314148a261a620a9c24ead394d436d5102
Simon Liebold <lieboldsimonpaul@gmail.com>
2025-05-21 20:24:13 +0300

    added getSchedule function

--- server/routes/schedules.js
+++ server/routes/schedules.js
@@ -1,23 +1,10 @@
-const { default: mongoose } = require("mongoose")
-const Schedule = require("../schemas/Schedule")
-
 module.exports = () => {
+  const mongoose = require("mongoose")
+  const Schedule = require("../schemas/Schedule")
   const router = require("express").Router()
   const roles = require("../roles")
 
-  // Get all
-  router.get("/", roles.requireAdmin, async (req, res, next) => {
-    try {
-      const schedules = await Schedule.find({})
-      if (!schedules) return next(new Error("Nicht gefunden"))
-      return res.send(schedules)
-    } catch (err) {
-      return next(err)
-    }
-  })
-
-  // Get one
-  router.get("/:id", roles.requireAdmin, async (req, res, next) => {
+  const getSchedule = async (req, res, next) => {
     try {
       const schedule = await Schedule.aggregate([
         { $match: { _id: new mongoose.Types.ObjectId(req.params.id) } },
@@ -31,16 +18,38 @@ module.exports = () => {
         },
       ])
       if (!schedule) return next(new Error("Nicht gefunden"))
-      return res.send(schedule[0])
+      req.schedule = schedule[0]
+      next()
+    } catch (err) {
+      return next(err)
+    }
+  }
+
+  // Get all
+  router.get("/", roles.requireAdmin, async (req, res, next) => {
+    try {
+      const schedules = await Schedule.find({})
+      if (!schedules) return next(new Error("Nicht gefunden"))
+      return res.send(schedules)
     } catch (err) {
       return next(err)
     }
   })
 
+  // Get one
+  router.get(
+    "/:id",
+    roles.requireAdmin,
+    getSchedule,
+    async (req, res, next) => {
+      return res.send(req.schedule)
+    }
+  )
+
   // Create one
   router.post("/", roles.requireAdmin, async (req, res, next) => {
     try {
-      const { short, title, employmentId, jobIds } = req?.body || {}
+      const { short, title } = req?.body || {}
       const schedule = new Schedule({
         short,
         title,
@@ -57,7 +66,7 @@ module.exports = () => {
   // Update one
   router.put("/:id", roles.requireAdmin, async (req, res, next) => {
     try {
-      const { short, title, employmentId, jobIds } = req?.body || {}
+      const { short, title } = req?.body || {}
       await Schedule.findByIdAndUpdate(req.params.id, {
         short,
         title,
